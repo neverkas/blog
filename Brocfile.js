dfcc65a2b94ed5caf1258646c566028c856bf84d
neverkas <almacenamiento1@gmail.com>
2015-06-18 19:35:11 -0300

    Se agregó materialize

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,8 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
+app.import("bower_components/materialize/bin/materialize.css");
+app.import("bower_components/materialize/bin/materialize.js");
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
